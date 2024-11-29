d89a3e450937a7c779bf69d6e6683ffee71f06a8
Doc-Franz <federico.franzi94@gmail.com>
2024-11-29 16:52:16 +0100

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MyNav from "./components/MyNav";
 import MyHero from "./components/MyHero";
 import MyMovies from "./components/MyMovies";
 import MyFooter from "./components/MyFooter";
-import MyAccount from "./components/MyAccount";
+// import MyAccount from "./components/MyAccount";
 
 function App() {
   return (
